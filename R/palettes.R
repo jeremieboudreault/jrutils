b9b9384e35b00b96734a1692251bfc035697fcd0
Jeremie Boudreault <jeremieboudreault11@gmail.com>
2023-05-03 09:49:27 -0400

    Renamed colors to palettes. Have two palettes for now : pal_rdb and pal_rbow.

---
+++ R/palettes.R
@@ -0,0 +1,31 @@
+#' Colors used within the `jtheme`
+#'
+#' * light_red :
+#' * light_blue :
+#' * red :
+#' * blue :
+#' * light_grey :
+#'
+#'
+#'@export
+pal_rdbu = list(
+    red        = "#d6604d",
+    light_red  = "#fddbc7",
+    light_grey = "#F7F7F7",
+    light_blue = "#d1e5f0",
+    blue       = "#4393c3",
+)
+
+#'@export
+pal_rbow = list(
+    yellow      = "#ffca3a",
+    orange      = "#ff924c",
+    red         = "#ff595e",
+    blue1       = "#1982c4",
+    blue2       = "#4267ac",
+    purple      = "#6a4c93",
+    green1      = "#8ac926",
+    green2      = "#52a675",
+    green3      = "#285943",
+    black       = "grey10"
+)
