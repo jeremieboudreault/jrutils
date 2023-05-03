09437e1753a53d852800c7351d64501d00de31db
Jeremie Boudreault <jeremieboudreault11@gmail.com>
2023-05-03 09:31:31 -0400

    Added two random plot for the plot_random() function.

--- R/plot_random.R
+++ R/plot_random.R
@@ -5,9 +5,19 @@
 #' `plot_random` takes a palette of colors of values and plot it.
 #'
 #' @export
-plot_random <- function() {
+plot_random <- function(id) {
 
-    ggplot(data = MASS::Cars93, aes(x = Price, y = MPG.city)) +
-    geom_point()
+    if (id == 1) {
+        return(
+            ggplot(data = MASS::Cars93, aes(x = Price, y = MPG.city, col = DriveTrain)) +
+            geom_point()
+        )
+    }
+    if (id == 2) {
+        return(
+            ggplot(data = MASS::Cars93, aes(x = Fuel.tank.capacity, y = Rear.seat.room, col = DriveTrain)) +
+            geom_point()
+        )
+    }
 
 }
